ce0a7594a0089df46106cfa00cf09c9bb3b50b9e
coderxie <321882110@qq.com>
2022-09-27 23:14:31 +0800

    生成crud模版

--- src/dao/UserDao.ts
+++
@@ -1,7 +0,0 @@
-import { BaseDao, Dao, Sql } from '@snow';
-
-@Dao('users')
-export class UserDao extends BaseDao {
-  @Sql('select * from users where `id` = ?')
-  async getUserById(id: number) {}
-}
