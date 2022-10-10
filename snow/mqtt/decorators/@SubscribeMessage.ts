881b056b88a095d673b27094c9025bfa6aa62260
coderxie <321882110@qq.com>
2022-10-10 22:57:49 +0800

    修复mqtt不能订阅多个主题的问题

--- snow/mqtt/decorators/@SubscribeMessage.ts
+++ snow/mqtt/decorators/@SubscribeMessage.ts
@@ -5,7 +5,7 @@ import { TopicsMap } from './types';
 export function SubscribeMessage(topicName: string): MethodDecorator {
   if (!topicName) throw new Error('订阅的topicName不能为空');
   return (target, methodName: any, descriptor: TypedPropertyDescriptor<any>) => {
-    const topicsMap: TopicsMap = Reflect.getOwnMetadata(META_KEYS.MASSAGE_DATA, target) || {};
+    const topicsMap: TopicsMap = Reflect.getOwnMetadata(META_KEYS.TOPICS, target) || {};
     topicsMap[topicName] = methodName;
     Reflect.defineMetadata(META_KEYS.TOPICS, topicsMap, target);
   };
